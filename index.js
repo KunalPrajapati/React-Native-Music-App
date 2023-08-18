865723bfc6057a3c02232d2c5c025cd8b7eafbe9
KunalPrajapati <kunalprajpati@gmail.com>
2023-08-19 04:03:50 +0530

    added all the files to git

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import { playbackService } from './musicPlayerService';
+
+
+TrackPlayer.registerPlaybackService(() => playbackService);
+
+
+AppRegistry.registerComponent(appName, () => App);
